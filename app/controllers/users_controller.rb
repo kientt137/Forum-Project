be7c187ed72c61e8b4681413eff5d653fa465b3b
Tran Trung Kien <kientt13.7@gmail.com>
2016-04-16 23:44:07 +0700

    Finish log ii log out

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,9 +10,9 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      log_in @user
       flash[:success] = "Sign up success! Welcome to forum"
       redirect_to @user
-      # Handle a successful save.
     else
       render 'new'
     end
