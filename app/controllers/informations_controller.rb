32e0fa16cb7be9da8a2d1c431d0fd28cd320616f
Tran Trung Kien <kientt13.7@gmail.com>
2016-04-17 21:17:11 +0700

    Change information

--- app/controllers/informations_controller.rb
+++ app/controllers/informations_controller.rb
@@ -1,4 +1,41 @@
 class InformationsController < ApplicationController
-  def show
+  before_action :logged_in_user, only: [:edit, :update]
+  before_action :correct_user,   only: [:edit, :update]
+
+  def edit
+    @user = current_user
+    @info = @user.information
+  end
+
+  def update
+    @user = User.find(params[:id])
+    @info = @user.information
+    if @info.update_attributes(info_params)
+      flash[:success] = "Profile updated"
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
+
+  private
+  def info_params
+    params.require(:information).permit(:sex, :birth, :address, :phone, :introduce)
+  end
+
+  def logged_in_user
+    unless logged_in?
+      flash[:danger] = "Please log in."
+      redirect_to login_url
+    end
+  end
+
+  def correct_user
+    @user = User.find(params[:id])
+    unless current_user?(@user)
+      redirect_to(root_url)
+      flash[:danger] = "Access Denied"
+    end
   end
 end
